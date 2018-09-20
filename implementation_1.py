043649a6bafd3710869c553c5a077d3f000b880a
Bjarki Baldvinsson <bjarkib16@ru.is>
2018-09-20 23:11:38 +0000

    implementation_1

--- implementation_1.py
+++ implementation_1.py
@@ -1,40 +1,76 @@
-# Markmið leiksins er að komast frá upphafsstað á endastað. Notandi er staddur í völundarhúsi og byrjar á staðsetningu sem við köllum herbergi 1, 1.
+# Markmið leiksins er að komast frá upphafsstað á endastað. Notandi er staddur í völundarhúsi og byrjar á staðsetningu sem við köllum herbergi 1.
 #þáttakandi ferðast um eitt herbergi í einu með hverri ákvörðun, 9 herbergi eru í völundarhúsinu. Notanda er gefið upplýsingar hvert hann getur farið í hverju herbergi fyrir sig.
 #Þegar þáttakandi hefur er kominn í lokaherbergið hefur hann sigrað leikinn
 
 # https://github.com/Bjarkibadda/TileTraveller.git
 
 staðsetning = 1
-
-while staðsetning !=7 :  
+while staðsetning !=7 :
     if staðsetning == 1 or staðsetning == 4 :
-        inntak=input("You can travel: (N)orth.: ")
-    elif staðsetning == 2:
-        inntak=input("You can travel: (N)orth or (E)east or (S)outh.")
-    elif staðsetning == 3 or staðsetning == 9:
-        inntak=input("You can travel: (E)east or (S)outh.")
-    elif staðsetning == 6:
-        inntak=input("You can travel: (E)east or (W)est.: ")
-    elif staðsetning == 5:
-        inntak=input("You can travel: (S)outh or (W)est.: ")
-    elif staðsetning == 8:
-        inntak=input("You can travel: (N)orth or (S)outh.: ")
+        print("You can travel: (N)orth.")
+        Direction = input("Direction: ")
+        while Direction != "N" and Direction != "n":
+            print("Not a valid direction!")
+            Direction=input("Direction: ")       
+        if Direction == "N" or Direction == "n":
+            staðsetning+=1
+        
 
-    
-    if inntak == "N" or inntak == "n":
-        if staðsetning == 1 or staðsetning == 2 or staðsetning == 4 or staðsetning == 8:
-            staðsetning +=1
+    if staðsetning == 2:
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+        Direction=input("Direction: ")
+        while Direction !="N" and Direction !="n" and Direction !="E" and Direction !="e" and Direction !="S" and Direction !="s":
+            print("Not a valid direction!")
+            Direction=input("Direction: ")
+        if Direction == "N" or Direction == "n":
+            staðsetning+=1    
+        elif Direction == "E" or Direction=="e":
+            staðsetning+=3
+        elif Direction =="S" or Direction=="s":
+            staðsetning-=1
 
-    if inntak == "S" or inntak =="s":
-        if staðsetning == 2 or staðsetning == 3 or staðsetning == 5 or staðsetning == 8 or staðsetning == 9:
-            staðsetning -=1
-
-    if inntak == "W" or inntak == "w":
-        if staðsetning == 5 or staðsetning == 6 or staðsetning == 9:
-            staðsetning -=3
-
-    if inntak == "E" or inntak== "e" :
-        if staðsetning ==2 or staðsetning ==3 or staðsetning ==6:
-            staðsetning +=3
-
+    if staðsetning == 3 :
+        print("You can travel: (E)ast or (S)outh.")
+        Direction=input("Direction: ")
+        while Direction !="E" and Direction !="e" and Direction !="S" and Direction !="s":
+            print("Not a valid direction!")
+            Direction=input("Direction: ")
+        if Direction == "E" or Direction == "e":
+            staðsetning+=3
+        elif Direction == "S" or Direction == "s":
+            staðsetning-=1
+        
+    if staðsetning == 5 or staðsetning== 9:
+        print("You can travel: (S)outh or (W)est.")
+        Direction=input("Direction: ")
+        while Direction !="S" and Direction !="s" and Direction !="W" and Direction !="w":
+            print("Not a valid direction!")
+            Direction=input("Direction: ")
+        if Direction == "S" or Direction == "s":
+            staðsetning-=1
+        elif Direction == "W" or Direction == "w":
+            staðsetning-=3
+        
+    if staðsetning == 6:
+        print("You can travel: (E)ast or (W)est.")
+        Direction=input("Direction: ")
+        while Direction !="E" and Direction !="e" and Direction !="W" and Direction !="w":
+            print("Not a valid direction!")
+            Direction=input("Direction: ")
+        if Direction == "E" or Direction == "e":
+                staðsetning+=3
+        elif Direction == "W" or Direction =="w":
+                staðsetning-=3
+        
+    if staðsetning == 8:
+        print("You can travel: (N)orth or (S)outh.")
+        Direction=input("Direction: ")
+        while Direction !="N" and Direction !="n" and Direction !="S" and Direction !="s":
+            print("Not a valid direction!")
+            Direction=input("Direction: ")
+        if Direction == "N" or Direction == "n":
+            staðsetning+=1
+        elif Direction == "S" or Direction == "s":
+            staðsetning-=1
+        
 print("Victory!")
